1047186e04af8488103d3ec83c5c46f756f32494
Sumedh Nimkarde <blesssumedh@gmail.com>
2017-10-29 12:57:23 +0530

    make basic prettifier

---
+++ index.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const fs = require('fs');
+const file = fs.readFileSync(process.argv[2]).toString();
+
+const rowArray = file.split('\n');
+let maxLengthOfCharacter = 0;
+
+rowArray.forEach((row) => {
+  let eachrow = '';
+  let columnArray = row.split(',');
+  findMaxLengthString();
+
+  // Loop over each entry in column to add to form a row string
+  columnArray.forEach((item) => {
+    eachrow += '|' + item;
+    eachrow = eachrow + Array(maxLengthOfCharacter-item.length).join(" ");
+  });
+
+  // printing the row
+  console.log(eachrow);
+  console.log(Array(columnArray.length * maxLengthOfCharacter).join("-"));
+});
+
+function findMaxLengthString() {
+  rowArray.forEach((row) => {
+    row.split(',').forEach((item) => {
+      if (item.length > maxLengthOfCharacter) {
+        maxLengthOfCharacter = item.length + 1;
+      }
+    });
+  });
+}
